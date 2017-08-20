796265dffd3621515f54d1688b6aad953d61b4c1
mark <mark@nowhere.com>
2017-08-20 18:40:10 -0400

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface MovieQuote {
+  movie: string;
+  quote: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  movieQuote: MovieQuote = {
+    movie : 'The Movie',
+    quote : '?????'
+  };
+
 }
